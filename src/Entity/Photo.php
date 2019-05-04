29cebac72513ce65d963c21158e5f8cbd6002898
aga <agnieszkasurma77@gmail.com>
2019-05-04 22:27:42 +0200

    Add Photo entitiy
    php bin/console make:entity Photo

---
+++ src/Entity/Photo.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Photo entity.
+ */
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Class Photo.
+ *
+ * @ORM\Entity(repositoryClass="App\Repository\PhotoRepository")
+ */
+class Photo
+{
+    /**
+     * Primary key.
+     *
+     * @var int
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(name="id_zdj", type="integer")
+     */
+    private $id;
+
+    /**
+     * Photo location.
+     *
+     * @var string
+     * @ORM\Column(name="nazwa_jpg", type="string", length=45)
+     */
+    private $location;
+
+    /**
+     * Product associated with this picture.
+     *
+     * @var string
+     * @ORM\ManyToOne(targetEntity="App\Entity\Product", inversedBy="photos")
+     * @ORM\JoinColumn(name="id_produktu", nullable=false)
+     */
+    private $product;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLocation(): ?string
+    {
+        return $this->location;
+    }
+
+    public function setLocation(string $location): self
+    {
+        $this->location = $location;
+
+        return $this;
+    }
+
+    public function getProduct(): ?Product
+    {
+        return $this->product;
+    }
+
+    public function setProduct(?Product $product): self
+    {
+        $this->product = $product;
+
+        return $this;
+    }
+}
--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -4,6 +4,8 @@
  */
 namespace App\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -49,6 +51,19 @@ class Product
      */
     private $category;
 
+    /**
+     * Product photos.
+     *
+     * @var \Collection|Photo[]
+     * @ORM\OneToMany(targetEntity="App\Entity\Photo", mappedBy="product", orphanRemoval=true)
+     */
+    private $photos;
+
+    public function __construct()
+    {
+        $this->photos = new ArrayCollection();
+    }
+
     public function getId(): ?int
     {
         return $this->id;
@@ -86,6 +101,37 @@ class Product
     public function setCategory(?Category $category): self
     {
         $this->category = $category;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Photo[]
+     */
+    public function getPhotos(): Collection
+    {
+        return $this->photos;
+    }
+
+    public function addPhoto(Photo $photo): self
+    {
+        if (!$this->photos->contains($photo)) {
+            $this->photos[] = $photo;
+            $photo->setProduct($this);
+        }
+
+        return $this;
+    }
+
+    public function removePhoto(Photo $photo): self
+    {
+        if ($this->photos->contains($photo)) {
+            $this->photos->removeElement($photo);
+            // set the owning side to null (unless already changed)
+            if ($photo->getProduct() === $this) {
+                $photo->setProduct(null);
+            }
+        }
 
         return $this;
     }
