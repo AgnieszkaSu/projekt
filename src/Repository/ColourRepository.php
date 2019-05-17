e94923c9d04c5cd90b6bd19bdd2dda15b2bbce2e
aga <agnieszkasurma77@gmail.com>
2019-05-17 19:22:41 +0200

    Add missing repositories

---
+++ src/Repository/ColourRepository.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Colour;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Symfony\Bridge\Doctrine\RegistryInterface;
+
+/**
+ * @method Colour|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Colour|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Colour[]    findAll()
+ * @method Colour[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class ColourRepository extends ServiceEntityRepository
+{
+    public function __construct(RegistryInterface $registry)
+    {
+        parent::__construct($registry, Colour::class);
+    }
+
+    // /**
+    //  * @return Colour[] Returns an array of Colour objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('c')
+            ->andWhere('c.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('c.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?Colour
+    {
+        return $this->createQueryBuilder('c')
+            ->andWhere('c.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
