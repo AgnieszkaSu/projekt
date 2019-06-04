c19ac92037c5d293201b429cba2106ba49bb69f3
aga <agnieszkasurma77@gmail.com>
2019-06-05 00:21:57 +0200

    Simplify IndexController

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -5,12 +5,10 @@
 
 namespace App\Controller;
 
-use App\Repository\TypeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Knp\Component\Pager\PaginatorInterface;
 
 /**
  * Class IndexController.
@@ -21,14 +19,12 @@ class IndexController extends AbstractController
      * Index action.
      *
      * @param \Symfony\Component\HttpFoundation\Request $request    HTTP request
-     * @param \App\Repository\TypeRepository            $repository Repository
-     * @param \Knp\Component\Pager\PaginatorInterface   $paginator  Paginator
      *
      * @return \Symfony\Component\HttpFoundation\Response HTTP response
      *
      * @Route("/", name="index")
      */
-    public function index(Request $request, TypeRepository $repository, PaginatorInterface $paginator): Response
+    public function index(Request $request): Response
     {
         return $this->forward('App\Controller\ProductController::index');
     }
